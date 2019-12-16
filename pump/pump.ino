5d2729accbabe87ab46924c6d8e458027c3487ef
Dmitrey76 <35530334+Dmitrey76@users.noreply.github.com>
2019-12-16 13:10:19 +0300

    Update pump.ino

--- pump/pump.ino
+++ pump/pump.ino
@@ -1,13 +1,13 @@
-int _gaugePins[10] = {10,9,8,7,6,5,4,3,2,1};
+int _gaugePins[10] = {11,10,9,8,6,5,4,3,2};
 const int _gaugeSize = 10;
 
-const int _relayPin = 0;
+const int _relayPin = 12;
 const int _sensorPin = 0;
-const int _deltaPreasure = 10;
+const int _deltaPreasure = 2;
 
 const unsigned long _delayShowTime = 3000;
 const unsigned long _workTime =  30000;
-const unsigned long _delayTime = 15000;
+const unsigned long _delayTime = 3000; 
 
 unsigned long startWork;
 unsigned long showTime;
@@ -15,10 +15,8 @@ unsigned long showTime;
 int preasureValue = 0; 
 int prevPreasureValue = 0; 
 
-unsigned long workTime;
-
-int minPreasure = 100;
-int maxPreasure = 135; //  из 1023
+int minPreasure = 220;
+int maxPreasure = 298; //  из 1023
 
 bool workInProgress = false;
 bool repeatWork = false;
@@ -27,20 +25,30 @@ void setup() {
 
   int a;
 
-  /* Serial.begin(9600); */
+  Serial.begin(9600);
 
   for (a = 0; a < _gaugeSize; a++) {
     pinMode (_gaugePins[a], OUTPUT);
     digitalWrite (_gaugePins[a], LOW);
   }
 
+/* int b = 0;
+ 
+for (b = 0; b < 14; b++) {
+   Serial.println(b);
+    pinMode (b, OUTPUT);
+   digitalWrite (b, HIGH);
+   delay (1000);
+   digitalWrite (b, LOW);
+   delay (2000);
+}*/
+
   pinMode (_relayPin, OUTPUT);
   digitalWrite (_relayPin, HIGH);
 
   startWork = 0;
   workInProgress = false;
-  repeatWork = false;
-  workTime = _workTime;
+  repeatWork = true;
 
 }
 
@@ -51,19 +59,23 @@ void ShowPreasure (int value) {
   int a;
 
   for (a = 0; a < _gaugeSize; a++) {
-    
-    digitalWrite (_gaugePins[a], ! (minPreasure + (a * stepG) >= value) );
-    
+
+    digitalWrite (_gaugePins[a], (minPreasure + (a * stepG) >= value) );
+  Serial.print ("Шкала: ");
+  Serial.println (minPreasure + a * stepG);
   }
 
   showTime = millis ();
-  
+
 }
 
 void loop() {
 
   prevPreasureValue = preasureValue;
   preasureValue = analogRead (_sensorPin);
+
+ /*Serial.print("Давление: ");
+ Serial.println(preasureValue);*/
 
   if ( abs(preasureValue - prevPreasureValue) > _deltaPreasure ) {
     ShowPreasure (preasureValue);
@@ -74,35 +86,32 @@ void loop() {
     workInProgress = true;
     startWork = millis ();
     repeatWork = false;
-    /*Serial.println ("on");
+    Serial.println ("on");
     Serial.println (preasureValue);
-    Serial.println (millis () - startWork);*/
+    Serial.println (millis () - startWork);
   };
 
-  if (workInProgress & ((millis () - startWork) > workTime)) {
+  if (workInProgress & ((millis () - startWork) > _workTime)) {
     digitalWrite (_relayPin, HIGH);
     workInProgress = false;
     repeatWork = true;
     startWork = millis ();
-    if (workTime < (_workTime * 2)) {
-      workTime = workTime + 1000;
-    }
-    /*Serial.println (preasureValue);
+    Serial.println (preasureValue);
     Serial.println ((millis () - startWork));
-    Serial.println ("off - TO");*/
+    Serial.println ("off - TO");
   };
 
   if (workInProgress & (preasureValue > maxPreasure)) {
     digitalWrite (_relayPin, HIGH);
     workInProgress = false;
-    /*Serial.println (preasureValue);
-    Serial.println ("off");*/
+    Serial.println (preasureValue);
+    Serial.println ("off");
   };
-  
+
   if (!workInProgress & !repeatWork & (millis() - showTime > _delayShowTime)) {
     ShowPreasure (0);
   };
-  
+
   delay (1000);
-  
+
 }
