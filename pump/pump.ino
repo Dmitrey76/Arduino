651b459f138603c2d45161d9481c2d23ce76b435
Dmitrey76 <35530334+Dmitrey76@users.noreply.github.com>
2019-12-27 22:02:10 +0300

    Версия рабочая, с индикацией.

--- pump/pump.ino
+++ pump/pump.ino
@@ -1,13 +1,14 @@
-int _gaugePins[10] = {11,10,9,8,6,5,4,3,2};
+int _gaugePins[10] = {11, 10,9,8,7,6,5,4,3,2};
 const int _gaugeSize = 10;
 
 const int _relayPin = 12;
 const int _sensorPin = 0;
-const int _deltaPreasure = 2;
+const int _deltaPreasure = 7;
+const int _maxTrysCount = 20;
 
 const unsigned long _delayShowTime = 3000;
-const unsigned long _workTime =  30000;
-const unsigned long _delayTime = 3000; 
+const unsigned long _workTime =  25000;
+const unsigned long _delayTime = 2000;
 
 unsigned long startWork;
 unsigned long showTime;
@@ -15,8 +16,11 @@ unsigned long showTime;
 int preasureValue = 0; 
 int prevPreasureValue = 0; 
 
-int minPreasure = 220;
-int maxPreasure = 298; //  из 1023
+unsigned long deltaCheck; 
+
+int minPreasure = 230;
+int maxPreasure = 285; //  из 1023
+int tryCount = 0;
 
 bool workInProgress = false;
 bool repeatWork = false;
@@ -25,30 +29,20 @@ void setup() {
 
   int a;
 
-  Serial.begin(9600);
+//  Serial.begin(9600);
 
   for (a = 0; a < _gaugeSize; a++) {
     pinMode (_gaugePins[a], OUTPUT);
     digitalWrite (_gaugePins[a], LOW);
   }
 
-/* int b = 0;
- 
-for (b = 0; b < 14; b++) {
-   Serial.println(b);
-    pinMode (b, OUTPUT);
-   digitalWrite (b, HIGH);
-   delay (1000);
-   digitalWrite (b, LOW);
-   delay (2000);
-}*/
-
   pinMode (_relayPin, OUTPUT);
-  digitalWrite (_relayPin, HIGH);
+  digitalWrite (_relayPin, LOW);
 
-  startWork = 0;
-  workInProgress = false;
-  repeatWork = true;
+  startWork = millis ();
+  workInProgress = true;
+  repeatWork = false;
+  deltaCheck = 0;
 
 }
 
@@ -59,36 +53,37 @@ void ShowPreasure (int value) {
   int a;
 
   for (a = 0; a < _gaugeSize; a++) {
-
-    digitalWrite (_gaugePins[a], (minPreasure + (a * stepG) >= value) );
-  Serial.print ("Шкала: ");
-  Serial.println (minPreasure + a * stepG);
+    
+    digitalWrite (_gaugePins[a], ! (minPreasure + (a * stepG) >= value) );
+//    Serial.print ("Шкала: ");
+//    Serial.println (minPreasure + a * stepG);
   }
 
   showTime = millis ();
-
+  
 }
 
 void loop() {
 
   prevPreasureValue = preasureValue;
   preasureValue = analogRead (_sensorPin);
+  
+//  Serial.print("Давление: ");
+//  Serial.println(preasureValue);
 
- /*Serial.print("Давление: ");
- Serial.println(preasureValue);*/
+// if ( (abs(preasureValue - prevPreasureValue) > _deltaPreasure ) & (millis () - deltaCheck > 1000)) {
+//    ShowPreasure (preasureValue);
+//    deltaCheck = millis ();
+//  };
 
-  if ( abs(preasureValue - prevPreasureValue) > _deltaPreasure ) {
-    ShowPreasure (preasureValue);
-  };
-
-  if ( ((preasureValue < minPreasure) || repeatWork) & !workInProgress & ((millis () - startWork) > _delayTime) ){
+  if ( ((preasureValue < minPreasure) || repeatWork) & !workInProgress & ((millis () - startWork) > _delayTime) & (tryCount <= _maxTrysCount) ){
     digitalWrite (_relayPin, LOW);
     workInProgress = true;
     startWork = millis ();
     repeatWork = false;
-    Serial.println ("on");
-    Serial.println (preasureValue);
-    Serial.println (millis () - startWork);
+//    Serial.println ("on");
+//    Serial.println (preasureValue);
+//    Serial.println (millis () - startWork);
   };
 
   if (workInProgress & ((millis () - startWork) > _workTime)) {
@@ -96,22 +91,28 @@ void loop() {
     workInProgress = false;
     repeatWork = true;
     startWork = millis ();
-    Serial.println (preasureValue);
-    Serial.println ((millis () - startWork));
-    Serial.println ("off - TO");
+//    Serial.println (preasureValue);
+//    Serial.println ((millis () - startWork));
+//    Serial.println ("off - TO");
+    tryCount++;
   };
 
   if (workInProgress & (preasureValue > maxPreasure)) {
     digitalWrite (_relayPin, HIGH);
     workInProgress = false;
-    Serial.println (preasureValue);
-    Serial.println ("off");
+ //   Serial.println (preasureValue);
+ //   Serial.println ("off");
+    tryCount = 0;
   };
-
+  
+  if (workInProgress || repeatWork) {
+    ShowPreasure (preasureValue);
+    }  
+    
   if (!workInProgress & !repeatWork & (millis() - showTime > _delayShowTime)) {
     ShowPreasure (0);
   };
-
-  delay (1000);
-
+  
+  delay (500);
+  
 }
